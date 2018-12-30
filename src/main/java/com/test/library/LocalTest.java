0a46bbb794d56376f80e54b887a23250dbcee61d
Admin <Admin@DESKTOP-4T367PV>
2018-12-30 16:46:38 +0800

    nothing

--- src/main/java/com/test/library/LocalTest.java
+++ src/main/java/com/test/library/LocalTest.java
@@ -1,24 +1,21 @@
 package com.test.library;
 
+import java.util.Arrays;
 import java.util.Locale;
 
-import org.apache.catalina.tribes.util.Arrays;
+public class LocalTest {
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		Locale locale = Locale.getDefault();
+		System.out.println(locale.getCountry());
+		System.out.println(locale.getDisplayCountry());
+		System.out.println(locale.getLanguage());
+		System.out.println(locale.getDisplayLanguage());
+		System.out.println(Arrays.toString(locale.getISOCountries()));
+		System.out.println(Arrays.toString(locale.getISOLanguages()));
+		System.out.println(Arrays.toString(locale.getAvailableLocales()));
 
-public class LocalTest
-{
-    @SuppressWarnings("static-access")
-    public static void main(String[] args)
-    {
-        Locale locale = Locale.getDefault();
-        System.out.println(locale.getCountry());
-        System.out.println(locale.getDisplayCountry());
-        System.out.println(locale.getLanguage());
-        System.out.println(locale.getDisplayLanguage());
-        System.out.println(Arrays.toString(locale.getISOCountries()));
-        System.out.println(Arrays.toString(locale.getISOLanguages()));
-        System.out.println(Arrays.toString(locale.getAvailableLocales()));
-        
-        System.out.println(">>>>*****************************************");
-        
-    }
+		System.out.println(">>>>*****************************************");
+
+	}
 }
